0991f3dc2a58d84b860ea3c8bf1ab71f7f947479
Nathaniel Baxter <nathaniel.baxter@gmail.com>
2013-10-22 18:06:07 +1100

    Updated settings files to work with Launchpad.exe for mod publishing. Added required preview.jpg file. Mod now updates the PhaseGateUserMixin.lua file, so all entities are considered for phasing.

--- GenerateMod.py
+++ GenerateMod.py
@@ -4,23 +4,32 @@
 
 import sys
 import os
+import re
 
+# Modification constants for the Alien.lua file.
 filename = "Alien.lua"
 scriptline = 'Script.Load("lua/PhaseGateUserMixin.lua")\n'
 addvarsline = "AddMixinNetworkVars(PhaseGateUserMixin, networkVars)\n"
 initline = "    InitMixin(self, PhaseGateUserMixin)\n"
 
+# Modification constants for the PhaseGateUserMixin.lua file.
+filename2 = "PhaseGateUserMixin.lua"
+regex = re.compile("GetEntitiesForTeamWithinRange\(\"PhaseGate\",\s+" +
+    "([^\s,]+),\s+([^\s,]+),\s+([^\s,]+)\)")
+replace = "GetEntitiesWithinRange(\"PhaseGate\", \\2, \\3)"
+
 # Check for correct arguments.
 if len(sys.argv) != 2:
-	print ("""Generates the 'Alien Phase Gate Use' mod's lua/Alien.lua file.
+	print ("""Generates the Alien.lua and PhaseGateUserMixin.lua files.
 Usage: python GenerateMod.py luadir
   -- luadir: Location of the folder containing the clean Alien.lua file.""")
 	exit()
 
 # Check that the clean file exists.
-if not os.path.exists(sys.argv[1] + "/" + filename):
-	print ("Alien.lua was not found in the given luadir.")
-	exit()
+if not os.path.exists(sys.argv[1] + "/" + filename) \
+    or not os.path.exists(sys.argv[1] + "/" + filename2):
+  print ("One or more required files were not found in the given luadir.")
+  exit()
 
 # Create the mod lua directory if needed.
 if not os.path.exists("mod/lua"):
@@ -29,6 +38,10 @@ if not os.path.exists("mod/lua"):
 # Delete any old Alien.lua file.
 if os.path.exists("mod/lua/" + filename):
   os.remove("mod/lua/" + filename)
+
+# Delete any old 
+if os.path.exists("mod/lua" + filename2):
+  os.remove("mod/lua/" + filename2)
 
 # Open the files to read/write
 fread = open(sys.argv[1] + "/" + filename, 'r')
@@ -55,6 +68,20 @@ for line in fread.readlines():
 fread.close()
 fwrite.close()
 
+# Open the files to read/write
+fread = open(sys.argv[1] + "/" + filename2, 'r')
+fwrite = open("mod/lua/" + filename2, 'w')
+
+num_replacements = 0
+for line in fread.readlines():
+  nline = re.sub(regex, replace, line)
+  if not nline == line:
+    num_replacements = num_replacements + 1
+  fwrite.write(nline)
+
+fread.close()
+fwrite.close()
+
 if not scriptload:
   print ("Failed to add Script.Load call")
 
@@ -63,3 +90,6 @@ if not addvars:
 
 if not init:
   print ("Failed to add InitMixin call")
+
+if num_replacements == 0:
+  print ("Failed to perform mixin code changes")
